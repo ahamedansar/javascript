6f363e210d00a49f878a3419d3639cade3e3acee
ahamedansar <ahamedansarnasim@gmail.com>
2025-08-27 12:55:32 +0530

    ansar

---
+++ array-iteration-method/for-each.js
@@ -0,0 +1,9 @@
+//execute a function for each elements in the array (does not return any value   )
+
+
+const numbers = [1,5,7,];
+
+numbers.forEach(num => {
+    console.log(num);
+})
+
---
+++ array-iteration-method/map.js
@@ -0,0 +1,5 @@
+
+//transform each elements and return a new array
+let numbers = [1,4,8,11];
+let doubles = numbers.map(num => num * 2);
+console.log(doubles);
